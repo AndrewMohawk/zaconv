3270a31fa1e9106913fe6546fc6878b24bf491ac
rc1140 <rc1140@republiccommandos.co.za>
2013-10-15 02:45:42 -0700

    Added numberin

--- badgeCode/build.bash
+++ badgeCode/build.bash
@@ -1,7 +1,10 @@
 #!/bin/bash
+badgeNumber=1338
 for i in {1..2}
 do
-	sed -i '' --expression="s/1338;/133$i;/" src/badge.ino
+	let badgeNumber++
+	echo "Build firmware for badge $badgeNumber"
+	sed -i '' --expression="s/1338;/$badgeNumber;/" src/badge.ino
 	ino build
 	cp /home/rc1140/dev/zaconv/badgeCode/.build/uno/firmware.hex firmware$i.hex
 	git reset --hard
